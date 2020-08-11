2bf395c0f4155bacdc311fa6e11016b38074715a
ConorBaumgart <conorbaumgart@Conors-MacBook-Pro.local>
2020-08-11 19:16:03 -0400

    Adds DayInTime component

--- src/components/GoogleMapContainer/GoogleMapContainer.js
+++ src/components/GoogleMapContainer/GoogleMapContainer.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { DayInTime } from '../DayInTime/DayInTime';
 
 export const GoogleMapContainer = () => {
     return (
         <div style={{height: '100vh', width: '100%'}}>
+            <DayInTime />
             <GoogleMapReact
                 bootstrapURLKeys={{ key: 'AIzaSyD4bqhCOPhLTodbwADYYwuPDOwC0tYHE_M' }}
                 defaultCenter={{
                     lat: 30,
                     lng: 20
-                  }}
+                }}
                 defaultZoom={1}
-            />
+            >
+            </GoogleMapReact>
         </div>
     )
 }
