adafab489c199367a7c3912321a98f8913997779
ConorBaumgart <conorbaumgart@Conors-MacBook-Pro.local>
2020-08-11 18:48:51 -0400

    Converts GoogleMapsContainert to a functional component

--- src/components/GoogleMapContainer/GoogleMapContainer.js
+++ src/components/GoogleMapContainer/GoogleMapContainer.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-export class GoogleMapContainer extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
-  render() {
+export const GoogleMapContainer = () => {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyD4bqhCOPhLTodbwADYYwuPDOwC0tYHE_M' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+        <div style={{height: '100vh', width: '100%'}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyD4bqhCOPhLTodbwADYYwuPDOwC0tYHE_M' }}
+                defaultCenter={{
+                    lat: 30,
+                    lng: 20
+                  }}
+                defaultZoom={1}
+            />
+        </div>
+    )
 }
