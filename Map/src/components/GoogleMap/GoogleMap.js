6415e58e098a614f4e5c76d33b5742592bbb6587
ConorBaumgart <conorbaumgart@Conors-MacBook-Pro.local>
2020-11-25 10:40:58 -0500

    Removes files?

--- Map/src/components/GoogleMap/GoogleMap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const GoogleMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-      return map;
-}
