78224f1f00ba7772be58286ec83f201fd27885ed
ConorBaumgart <conorbaumgart@Conors-MacBook-Pro.local>
2020-11-25 10:56:15 -0500

    Adds Map folder back

---
+++ Map/src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const GoogleMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+      return map;
+}
